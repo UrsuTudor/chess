94ad745d785d838ba0b8a409aa139a4026151028
UrsuTudor <tudor_ursu90@yahoo.com>
2024-06-17 20:00:27 +0300

    Add king moves
    It uses the same steps as the knight, but spread over multiple methods
    to reduce abc size.

--- lib/king.rb
+++ lib/king.rb
@@ -9,4 +9,24 @@ class King < Piece
   end
 
   attr_reader :white, :black, :player
+
+  def possible_moves(board)
+    king_moves = moves_on_lower_row + moves_on_same_row + moves_on_upper_row
+
+    in_bounds_moves = exclude_out_of_bounds_moves(king_moves)
+
+    in_bounds_moves.delete_if { |move| allied_piece?(board, move[0], move[1]) }
+  end
+
+  def moves_on_upper_row
+    [[row + 1, col - 1], [row + 1, col], [row + 1, col + 1]]
+  end
+
+  def moves_on_same_row
+    [[row, col - 1], [row, col + 1]]
+  end
+
+  def moves_on_lower_row
+    [[row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+  end
 end
