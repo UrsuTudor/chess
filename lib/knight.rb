55f1cf3dea1d8dfdac899c35fb54edf2c0a460fb
UrsuTudor <tudor_ursu90@yahoo.com>
2024-06-17 19:29:01 +0300

    Added knight moves
    The method works by calculating all the hypothetical moves a knight could
    make, removing the ones that would be out of bounds and iterating through
    the in-bounds moves to remove the spaces blocked by an allied piece.

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,4 +9,13 @@ class Knight < Piece
   end
 
   attr_reader :white, :black, :player
+
+  def possible_moves(board)
+    knight_moves = [[row + 1, col + 2], [row - 1, col + 2], [row + 2, col + 1], [row - 2, col + 1], [row + 1, col - 2],
+                    [row - 1, col - 2], [row + 2, col - 1], [row - 2, col - 1]]
+
+    in_bounds_moves = exclude_out_of_bounds_moves(knight_moves)
+
+    in_bounds_moves.delete_if { |move| allied_piece?(board, move[0], move[1]) }
+  end
 end
